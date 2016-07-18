fef34fb665896a782ad6c3bf288f79f85953e22a
Steve Bauman <steven_bauman@outlook.com>
2016-07-18 12:14:14 -0400

    Added more tests

--- tests/SettingTest.php
+++ tests/SettingTest.php
@@ -4,7 +4,6 @@ namespace Larapacks\Setting\Tests;
 
 use Illuminate\Database\Eloquent\Model;
 use Larapacks\Setting\Contracts\Setting as SettingContract;
-use Larapacks\Setting\Facades\Setting;
 
 class SettingTest extends TestCase
 {
@@ -15,28 +14,32 @@ class SettingTest extends TestCase
 
     public function test_set()
     {
-        Setting::set('key', 'value');
+        setting()->set('key', 'value');
 
-        $this->seeInDatabase('settings', [
-            'key'   => 'key',
-            'value' => serialize('value'),
-        ]);
+        /** @var Model $setting */
+        $setting = setting()->model()->first();
+
+        $this->assertEquals('key', $setting->key);
+        $this->assertEquals('value', $setting->value);
+    }
+
+    public function test_is_encrypted()
+    {
+        $this->test_set();
+
+        $setting = setting()->model()->first();
+
+        $this->assertEquals('value', unserialize(decrypt($setting->getOriginal('value'))));
     }
 
     public function test_set_update()
     {
-        Setting::set('key', 'value');
+        setting()->set('key', 'value');
 
-        Setting::set('key', 'updated');
+        setting()->set('key', 'updated');
 
         $this->seeInDatabase('settings', [
             'key'   => 'key',
-            'value' => serialize('updated'),
-        ]);
-
-        $this->dontSeeInDatabase('settings', [
-            'key'   => 'key',
-            'value' => serialize('value'),
         ]);
     }
 
@@ -44,27 +47,27 @@ class SettingTest extends TestCase
     {
         $this->test_set();
 
-        $this->assertEquals('value', Setting::get('key'));
+        $this->assertEquals('value', setting()->get('key'));
     }
 
     public function test_get_default()
     {
-        $this->assertEquals('default', Setting::get('non-existent-key', 'default'));
+        $this->assertEquals('default', setting()->get('non-existent-key', 'default'));
     }
 
     public function test_find()
     {
         $this->test_set();
 
-        $this->assertInstanceOf(Model::class, Setting::find('key'));
-        $this->assertNull(Setting::find('test'));
+        $this->assertInstanceOf(Model::class, setting()->find('key'));
+        $this->assertNull(setting()->find('test'));
     }
 
     public function test_has()
     {
         $this->test_set();
 
-        $this->assertTrue(Setting::has('key'));
+        $this->assertTrue(setting()->has('key'));
     }
 
     public function test_all()
@@ -75,9 +78,9 @@ class SettingTest extends TestCase
             'key.3' => 'value',
         ];
 
-        Setting::set($settings);
+        setting()->set($settings);
 
-        $this->assertEquals($settings, Setting::all()->toArray());
+        $this->assertEquals($settings, setting()->all()->toArray());
     }
 
     public function test_helper()
@@ -128,5 +131,12 @@ class SettingTest extends TestCase
         setting()->disable('new-key');
 
         $this->assertFalse(setting()->get('new-key'));
+    }
+
+    public function test_calling_on_model()
+    {
+        setting()->set('key', 'value');
+
+        $this->assertTrue(setting()->first()->exists);
     }
 }
